f30db94a4aa22f3cd7815e2d352ef4ef0f07450a
thisisthesarath <149189766+thisisthesarath@users.noreply.github.com>
2024-02-24 07:11:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,313 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from "react";
+import "./App.css";
+
+function App() {
+  return (
+      <div className="div">
+        <div className="div-2">
+          <div className="div-3">
+            <div className="column">
+              <div className="div-4">Agricultural Hub</div>
+            </div>
+            <div className="column-2">
+              <div className="div-5">
+                <div className="div-6">
+                  <div className="div-7">Home</div>
+                  <div className="div-8" />
+                </div>
+                <div className="div-9">About Us</div>
+                <div className="div-10">Marketplace</div>
+              </div>
+            </div>
+            <div className="column-3">
+              <div className="div-11">
+                <div className="div-12">Login</div>
+                <div className="div-13">Sign Up</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-14">
+          <img
+            loading="lazy"
+            srcSet="..."
+            className="img"
+          />
+          <div className="div-15">
+            <div className="div-16">
+              The Role of Value Addition in Revolutionizing Agriculture
+            </div>
+            <div className="div-17">
+              Li Europan lingues es membres del sam familie. Lor separat
+              existentie es un myth. Por scientie, musica, sport etc, litot
+              Europa usa li sam vocabular.
+            </div>
+            <div className="div-18">Get Started</div>
+          </div>
+        </div>
+        <div className="div-19">
+          <div className="div-20">
+            <div className="column-4">
+              <div className="div-21">
+                <div className="div-22">
+                  Our Passion for Agriculture Nurturing Growth and Sustaining
+                  the Future
+                </div>
+                <div className="div-23">
+                  Li Europan lingues es membres del sam familie. Lor separat
+                  existentie es un myth. Por scientie, musica, sport etc, litot
+                  Europa usa li sam vocabular.
+                </div>
+                <div className="div-24">Get Started</div>
+              </div>
+            </div>
+            <div className="column-5">
+              <img
+                loading="lazy"
+                srcSet="..."
+                className="img-2"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="div-25">What we do?</div>
+        <div className="div-26">
+          Li Europan lingues es membres del sam familie. Lor separat existentie
+          es un myth Por scientie, musica.
+        </div>
+        <div className="div-27">
+          <div className="div-28">
+            <div className="column-6">
+              <div className="div-29">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/88bd1ebf5ad59a6f439b60ac75780f0e757e77262479c976f1ee8593db769fa0?"
+                  className="img-3"
+                />
+                <div className="div-30">Value Addition Awareness</div>
+              </div>
+            </div>
+            <div className="column-7">
+              <div className="div-31">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/ee03e977485eaee15f008c3ae74455f256f04ada0f18a945e22fd6d44767ac65?"
+                  className="img-4"
+                />
+                <div className="div-32">Value Addition Guide</div>
+              </div>
+            </div>
+            <div className="column-8">
+              <div className="div-33">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/26619296f03a00e9d69bd808c25a46af61238b7b128c6dfbc220ac2825c89e35?"
+                  className="img-5"
+                />
+                <div className="div-34">Connecting Farmers</div>
+              </div>
+            </div>
+            <div className="column-9">
+              <div className="div-35">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/e2dbff3cdae3f4828ee9f1efab84ae7545a872d679a33ecc7cc6133aa956a63b?"
+                  className="img-6"
+                />
+                <div className="div-36">Nearby Crops</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-37">
+          <img
+            loading="lazy"
+            srcSet="..."
+            className="img"
+          />
+          <div className="div-38">
+            <div className="div-39">
+              Value Addition Farming Meets Sustainability: Building a Greener
+              Future
+            </div>
+            <div className="div-40">
+              <div className="div-41">Get Started</div>
+              <div className="div-42">Contact Us</div>
+            </div>
+          </div>
+        </div>
+        <div className="div-43">Contact Us!</div>
+        <div className="div-44">
+          <div className="div-45">
+            <div className="column-10">
+              <div className="div-46">
+                <div className="div-47">The Benefits of Using Agri-Hub</div>
+                <div className="div-48">
+                  Li Europan lingues es membres del sam familie. Lor separat
+                  existentie es un myth. Por scientie, musica, sport etc, litot
+                  Europa usa li sam vocabular.{" "}
+                </div>
+                <div className="div-49">Get Started</div>
+              </div>
+            </div>
+            <div className="column-11">
+              <div className="div-50">
+                <div className="div-51">
+                  <div className="div-52">
+                    <div className="div-53">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/4db6b5383e37b0c647698421ce3f3bb1f6ddc2c21f9796b39e6f7bfc692808ba?"
+                        className="img-7"
+                      />
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/14babc9b6af12d77aa99774d92e2c9975f181c47171bacb3b91d6bac463df1b6?"
+                        className="img-8"
+                      />
+                    </div>
+                    <div className="div-54">
+                      <div className="div-55">15+</div>
+                      <div className="div-56">Years Of Experienced Farmers</div>
+                      <div className="div-57">40+</div>
+                      <div className="div-58">Product</div>
+                    </div>
+                  </div>
+                  <div className="div-59">
+                    <img
+                      loading="lazy"
+                      srcSet="..."
+                      className="img-9"
+                    />
+                    <div className="div-60">
+                      <div className="div-61">2,458+</div>
+                      <div className="div-62">Satisfied Farmers</div>
+                    </div>
+                  </div>
+                  <div className="div-63">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/3abfe19c63c9ad11372b5c17f5d526d59e3e169b0a69f8afc97314f0687f67d9?"
+                      className="img-10"
+                    />
+                    <div className="div-64">
+                      <div className="div-65">36</div>
+                      <div className="div-66">Local Team Members</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="testimonials">Testimonials </div>
+        <div className="div-67">
+          Li Europan lingues es membres del sam familie. Lor separat existentie
+          es un myth. Por scientie, musica, sport etc, litot Europa usa li sam
+          vocabular.{" "}
+        </div>
+        <div className="div-68">
+          <div className="div-69">
+            <div className="div-70">
+              <div className="column-12">
+                <div className="div-71">
+                  <div className="div-72">
+                    <img
+                      loading="lazy"
+                      srcSet="..."
+                      className="img-11"
+                    />
+                    <div className="div-73">
+                      <div className="div-74">ChiChi</div>
+                      <div className="div-75">Tiruppur</div>
+                    </div>
+                  </div>
+                  <div className="div-76">
+                    I sold my value added coconut for 100 rupees each!
+                  </div>
+                </div>
+              </div>
+              <div className="column-13">
+                <div className="div-77">
+                  <div className="div-78">
+                    <img
+                      loading="lazy"
+                      srcSet="..."
+                      className="img-12"
+                    />
+                    <div className="div-79">
+                      <div className="div-80">Queen Rita </div>
+                      <div className="div-81">Chennai</div>
+                    </div>
+                  </div>
+                  <div className="div-82">
+                    Agri-Hub helped me to reach the potential farmers to buy
+                    vegetables for my store!
+                  </div>
+                </div>
+              </div>
+              <div className="column-14">
+                <div className="div-83">
+                  <div className="div-84">
+                    <img
+                      loading="lazy"
+                      srcSet="..."
+                      className="img-13"
+                    />
+                    <div className="div-85">
+                      <div className="div-86">Gloria Uko</div>
+                      <div className="div-87">Madurai</div>
+                    </div>
+                  </div>
+                  <div className="div-88">
+                    The experienced farmer guided and helped me to achieve value
+                    addition, Thanks to Agri- Hub!
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-89">Contact us if you have any Queries!</div>
+        <div className="div-90">Your full name</div>
+        <div className="div-91">Your email address</div>
+        <div className="div-92">Write a note about your request</div>
+        <div className="div-93">Send</div>
+        <div className="div-94">
+          <div className="div-95">
+            <div className="div-96">
+              <div className="div-97">AgriVista Farms</div>
+              <div className="div-98">
+                Li Europan lingues es membres del sam familie. Lor separat
+                existentie{" "}
+              </div>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/4401ad698ee7ce58db0c745188fe30f973869d84beaf0287e8a797e6ee723bec?"
+                className="img-14"
+              />
+            </div>
+            <div className="div-99">
+              <div className="div-100">
+                <div className="div-101">Pages</div>
+                <div className="div-102">Home</div>
+                <div className="div-103">About</div>
+                <div className="div-104">Product</div>
+              </div>
+              <div className="div-105">
+                <div className="div-106">About</div>
+                <div className="div-107">Testimonials</div>
+                <div className="div-108">Contact Us</div>
+                <div className="div-109">Benefit</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+            
+  );
+}
+
+export default App;
